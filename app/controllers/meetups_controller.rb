ac5786a2277c53ab97088949fc4ad13299c97a29
Dominik Zduńczyk <dominik.zdunczyk@gmail.com>
2018-10-06 13:34:52 +0200

    Added CRUD of meetup.

--- app/controllers/meetups_controller.rb
+++ app/controllers/meetups_controller.rb
@@ -1,13 +1,34 @@
 class MeetupsController < ApplicationController
 
   def index
-    @meetups = Meetup.all
-    render json: @meetups
+    meetups = Meetup.all
+    render json: {status: 'SUCCESS', message: 'Loaded meetups', data: meetups}, status: :ok
   end
 
-  def new
+  def show
+    meetup = Meetup.find(params[:id])
+    render json: {status: 'SUCCESS', message: 'Loaded meetup', data: meetup}, status: :ok
   end
 
-  def delete
+  def create
+    meetup = Meetup.new(meetup_params)
+
+    if meetup.save
+      render json: {status: 'SUCCESS', message: 'Meetup added', data: meetup}, status: :created
+    else
+      render json: {status: 'ERROR', message: 'Meetup not added', data: meetup.errors}, status: :bad_request
+    end
+  end
+
+  def destroy
+    meetup = Meetup.find(params[:id])
+    meetup.destroy
+    render json: {status: 'SUCCESS', message: 'Meetup deleted', data: meetup}, status: :ok
+  end
+
+  private
+
+  def meetup_params
+    params.permit(:name , :description, :evaluation_date, :latitude, :longtiude)
   end
 end
