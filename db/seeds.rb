ed88c38d836d3f95ca1635a64af46b70892487fe
Dominik Zduńczyk <dominik.zdunczyk@gmail.com>
2018-10-06 12:21:11 +0200

    Added seed of creation meetups

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+Meetup.create!(:name => 'Scala meeting', :description => 'It is an sample awesome Scala meeting', :evaluation_date => 1538819575, :latitude => 53.1274275, :longtiude => 23.1575406)
+Meetup.create!(:name => 'Ruby meeting', :description => 'It is an sample awesome Ruby meeting', :evaluation_date => 1538819576, :latitude => 53.1174275, :longtiude => 23.1375406)
+Meetup.create!(:name => 'Meet.js', :description => 'It is an sample awesome Javascript meeting', :evaluation_date => 1538819577, :latitude => 53.15754006, :longtiude => 23.1575406)
+
+p "Created #{Meetup.count} meetups"
