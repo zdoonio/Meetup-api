082cae7143e79ab15302bbe0d733066718b481fe
Dominik Zduńczyk <dominik.zdunczyk@goodsoft.pl>
2018-10-11 11:32:46 +0200

    Added namespace to api/v1.

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,19 @@
 Rails.application.routes.draw do
-  devise_for :users,
-             path: '',
-             path_names: {
-                 sign_in: 'login',
-                 sign_out: 'logout',
-                 registration: 'signup'
-             },
-             controllers: {
-                 sessions: 'sessions',
-                 registrations: 'registrations'
-             }
-  resources :meetups
+  namespace :api do
+    namespace :v1 do
+    devise_for :users,
+               path: '',
+               path_names: {
+                   sign_in: 'login',
+                   sign_out: 'logout',
+                   registration: 'signup'
+               },
+               controllers: {
+                   sessions: 'sessions',
+                   registrations: 'registrations'
+               }
+    resources :meetups
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
